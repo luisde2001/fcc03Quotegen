b11ca27c4bea73c6aa35f352a02e13df46cd6e17
luisde2001@yahoo.com <luisde2001@yahoo.com>
2017-02-14 16:03:41 +0100

    Complete change of message when pressing button. Jquery based. Important: scr script javascript.js must be after jquery import, just before ending body

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-<script>
+
   $(document).ready(function() {
-    // Only change code below this line.
+   
     $("#getQuote").on("click", function(){
-    	$(".quote").html("Here is the message");
-});
-    // Only change code above this line.
-  });
-</script>
+    	$(".quote").html("Here is the quote");
+	});//Ends on click function
+    
+  });//Ends ready function
